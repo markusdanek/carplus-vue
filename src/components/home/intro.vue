a6981ed1071a68038aaec9e57f8915fa8ea86d45
Markus Danek <markus.danek-cic@ibm.com>
2017-11-16 15:44:22 +0100

    Working on press component for home

--- src/components/home/intro.vue
+++ src/components/home/intro.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home-intro">
-    <div class="top">
-		    <div class="sub"></div>
+
         <b-col class="home-intro-text" offset="1">
           <b-row>
             <b-col sm="12" md="3" lg="2">
@@ -21,7 +20,6 @@
             </b-col>
           </b-row>
         </b-col>
-	  </div>
   </div>
 </template>
 
@@ -35,37 +33,12 @@
   @import "../../styles/util/util.scss";
 
   .home-intro {
+    background: $color-white;
     .row {
       @include rem((margin: 0));
     }
-    .top {
-      overflow: hidden;
-      max-width: 100%;
-      min-height: 650px;
-      background-image: linear-gradient(-180deg, #FFFFFF 0%, #F5F8FC 100%);
-      box-shadow: inset 0 -10px 20px -10px rgba(0, 0, 0, 0.05);
-      width: 100%;
-      margin-left: 50%;
-      -ms-transform: translateX(-50%);
-      transform: translateX(-50%);
-      .sub {
-        overflow: hidden;
-        bottom: -48%;
-        height: 100%;
-        background: rgba(228, 234, 240, 0.3);
-        border: 1px solid #E4EAF0;
-        opacity: 1;
-        position: absolute;
-        right: -50%;
-        width: 150%;
-        -ms-transform: rotate(-12.5deg);
-        transform: rotate(-12.5deg);
-        z-index: -99;
-      }
-    }
     .home-intro-text {
       @include rem((padding: 80px 0));
-      z-index: 99;
       h1 {
         @include rem((padding-bottom: 15px));
         @include rem(font-size, 46px);
--- src/components/home/press.vue
+++ src/components/home/press.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home-press">
     <b-col sm="10" offset="1">
-      <h1>Unsere zuletzt veröffentlichte Presseartikeln</h1>
+      <h1>Letzte Meldungen</h1>
       <b-row>
-          <b-col lg="3" class="item" v-for="article in articles" :key="article.id">
+          <b-col lg="4" class="item" v-for="article in articles" :key="article.id">
             <div class="image">
               <img :src="article.img" alt="img">
             </div>
             <b-col sm="10" class="text">
-              <h3>{{article.title}}</h3>
-              <p>{{article.desc}}</p>
+              <div class="title">
+                <h3>{{article.title}}</h3>
+              </div>
+              <div class="desc">
+                <p>{{article.desc}}</p>
+              </div>
+              <div class="date">
+                <p>{{article.date}}</p>
+              </div>
             </b-col>
         </b-col>
       </b-row>
+      <div class="more">
+        <b-button :size="size" :variant="outline-success" href="#">
+          Mehr Meldungen
+        </b-button>
+      </div>
     </b-col>
   </div>
 </template>
@@ -25,8 +37,8 @@
         articles: [
           {
             id: 1,
-            title: 'Wechsel in der carplus-Geschäftsleitung',
-            desc: 'Lesen Sie hier mehr!',
+            title: 'Neu seit November',
+            desc: 'Vorteilhafte Wrack-Ankaufsoption für carplus-Partner',
             date: 'November 2017',
             img: 'https://blog.frame.io/wp-content/uploads/2017/11/headphones-video-editor-696x414.jpg'
           },
@@ -43,14 +55,14 @@
             desc: 'REISEKASKO gleich hier online abschließen!',
             date: 'Oktober 2017',
             img: 'https://blog.frame.io/wp-content/uploads/2017/10/walmart-vertical-696x414.jpg'
-          },
-          {
-            id: 4,
-            title: 'Wechsel in der carplus-Geschäftsleitung',
-            desc: 'Lesen Sie hier mehr!',
-            date: 'September 2017',
-            img: 'https://blog.frame.io/wp-content/uploads/2017/11/17-10-29-palm-trees-header-696x414.jpg'
           }
+          // {
+          //   id: 4,
+          //   title: 'Wechsel in der carplus-Geschäftsleitung',
+          //   desc: 'Ab sofort übernimmt Frau Mag. (FH) Stiller die neue Position ein',
+          //   date: 'September 2017',
+          //   img: 'https://blog.frame.io/wp-content/uploads/2017/11/17-10-29-palm-trees-header-696x414.jpg'
+          // }
         ]
       }
     }
@@ -60,15 +72,14 @@
 <style scoped lang="scss">
   @import "../../styles/util/util.scss";
   .home-press {
-    // @include rem((padding: 60px 0));
-    background: rgb(238, 242, 247);
+    @include rem((padding: 80px 0));
+    background: #F3F4F6;
     h1 {
-      @include rem((margin-bottom: 20px));
-      border-bottom: 5px solid $color-red-carplus;
+      @include rem((margin-bottom: 40px));
       font-family: 'FFDINWeb';
       font-weight: bold;
       display: inline-block;
-      color: $color-red-carplus;
+      color: $color-black;
     }
     .item {
       .image {
@@ -80,16 +91,28 @@
       .text {
         @include rem((padding: 20px));
         background: $color-white;
-        // min-height: 200px;
         position: relative;
         top: -50px;
-        h3 {
-          font-weight: bold;
+        text-align: center;
+        .title {
+          h3 {
+            font-weight: bold;
+          }
         }
-        p {
-          text-decoration: underline;
+        .desc {
+          p {
+
+          }
+        }
+        .date {
+          p {
+            @include rem(font-size, 14px);
+          }
         }
       }
+    }
+    .more {
+      text-align: right;
     }
   }
 </style>
